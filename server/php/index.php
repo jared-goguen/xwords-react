85acb820c7c3e31cc37aebd305ecf3af2fa63983
jared <jared.goguen@gmail.com>
2018-06-12 17:40:31 -0400

    added css back into index.php for a minute

--- server/php/index.php
+++ server/php/index.php
@@ -2,6 +2,7 @@
   <head>
     <title><?php echo config('name'); ?></title>
     <link rel='stylesheet' type='text/css' href="<?php echo q_path('index.css'); ?>" >
+    <link rel='stylesheet' type='text/css' href="<?php echo q_path('puzzle.css'); ?>" >
     <link rel='stylesheet' type='text/css' href="<?php echo q_path('webpack.css'); ?>" >
   </head>
   <body>
